234c2ba529e1639d79718ae67c1eb8da206866c0
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-12-26 04:57:49 -0500

    Create project_decorator.py

---
+++ Proyecto con decoradores/project_decorator.py
@@ -0,0 +1,24 @@
+""" Project to run decorators """
+
+class MyClass:
+    """ Main class"""
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def say_hello(self):
+        """ Say Hello"""
+        print(f'Hi {self.username}, welcome to our system!')
+
+    def show_password(self):
+        """ Show password """
+        print(f'Hi {self.username}, your password starts by: {self.password[:4]}{len(self.password[4:])*"*"}')
+
+
+
+my_class = MyClass('carogomez', 'testpwd123.')
+print(my_class.username)
+print(my_class.password)
+my_class.say_hello()
+my_class.show_password()
